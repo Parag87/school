bc09a8f2f63b87a6cfcf3ed3bad32e6bc11548c8
hemsundar9 <hemsundar9>
2017-01-30 14:13:34 +0530

    klass work with key

--- src/main/resources/db/migration/V3__create_teachers_schema.sql
+++ src/main/resources/db/migration/V3__create_teachers_schema.sql
@@ -4,6 +4,4 @@ CREATE TABLE `teachers` (
   `name` VARCHAR(45) NOT NULL,
   `age` INT NOT NULL,
   `gender` ENUM('M', 'F') NOT NULL,
-  `created`    TIMESTAMP      NOT NULL  DEFAULT NOW(),
-  `modified`   TIMESTAMP      NOT NULL  DEFAULT NOW(),
   PRIMARY KEY (`id`));
